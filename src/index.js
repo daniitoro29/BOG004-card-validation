4da334e9c6d0da4691f9fd83e45ab4233fb07cf4
Daniela Toro <daniitoro29@gmail.com>
2022-01-25 12:22:05 -0500

    Cambios en condicionales pares e impares

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+document.getElementById("btnValidation").addEventListener("click", () => {
+  let tcNumber = document.getElementById("numberCard").value;
+  if (document.getElementById("numberCard").value === "") {
+    alert("Upssss, no has ingresado tu TC")
+  }else{
+    validator.isValid(tcNumber);
+
+  }
+  console.log("eL VALOR ES",tcNumber);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
-};
-
+  isValid: (creditCardNumber) => {
+    console.log("El valor de tu TC es ", creditCardNumber);
+    let creditCardNumberArray = creditCardNumber.split("");
+/*     creditCardNumberArray.reverse();
+    console.log(creditCardNumberArray); */
+    let creditCardNumberArrayLength = creditCardNumberArray.length;
+    let creditCardNumberInvertido = []
+    for(let i = creditCardNumberArrayLength - 1 ; i >= 0; --i){
+      creditCardNumberInvertido.push(creditCardNumberArray[i]);
+    }
+   /*  console.log(creditCardNumberInvertido); */
+    for (let i = 0 ; i < creditCardNumberInvertido.length; i++){
+      if (i % 2 == 0) { 
+        creditCardNumberInvertido.push(creditCardNumberInvertido [i] * 2);
+        console.log(creditCardNumberInvertido);
+      }else{
+        console.log("El número no es par");
+      }
+    }
+  }
+}
 export default validator;
+/* function viewsArrayImput(){
+  let arrayInput = new Array();
+  let inputsValues = document.getElementById("numberCard");
+  namesValues = [].map.call(inputsValues,function(dataInput){
+    arrayInput.push(dataInput.value);
+  });  
+  arrayInput.forEach(function(inputsValuesData){
+    console.log("El dato es" + arrayInput);
+  });
+} */
+/* let creditDate =  document.getElementById("numberCard").value;
+const myArr = Array.from (creditDate); */
+/* const isValid = [document.getElementById("numberCard").value];
+console.log(isValid); 
+isValid */
+/* {() => saveCard} */
+
+/* const validator = ()=> {
+  const saveCard =()=> {
+    let cardNumber = document.getElementById("numberCard").value;
+    let arrayCard = cardNumber.split("");
+    console.log("Hola Daniela");
+  }
+}
+export default validator; */
